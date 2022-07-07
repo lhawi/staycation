c04d21ccb86f10b8042ca4354472b5aae2701192
Lukman Hawi <lukmanhawi@gmail.com>
2022-07-07 10:38:51 +0700

    Initial commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import modulName from "elements/Button";
+
+export default class LandingPage extends Component {
+  render() {
+    return <div>LandingPage</div>;
+  }
+}
