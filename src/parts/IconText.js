9761ee3d5208f9b35448769b21a2a7a1e7b4060b
Lukman Hawi <lukmanhawi@gmail.com>
2022-10-15 08:53:38 +0700

    add component header

---
+++ src/parts/IconText.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function () {
+  return (
+    <Button className="brand-name-icon" href="" type="link">
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  );
+}
